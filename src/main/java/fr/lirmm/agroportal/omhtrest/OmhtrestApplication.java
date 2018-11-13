049ae73e4877972f7c076efe0f97f886d366df24
elcioabrahao <elcio.abrahao@gmail.com>
2018-11-13 14:56:49 +0100

    Initial commit

---
+++ src/main/java/fr/lirmm/agroportal/omhtrest/OmhtrestApplication.java
@@ -0,0 +1,12 @@
+package fr.lirmm.agroportal.omhtrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmhtrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmhtrestApplication.class, args);
+    }
+}
---
+++ src/test/java/fr/lirmm/agroportal/omhtrest/OmhtrestApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.lirmm.agroportal.omhtrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OmhtrestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
