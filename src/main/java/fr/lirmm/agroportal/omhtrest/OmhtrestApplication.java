c5a239f45c65cd643d2f5fa9a6fb64b57ec19c9e
elcioabrahao <elcio.abrahao@gmail.com>
2018-11-13 15:02:18 +0100

    Test new project location.

--- src/main/java/fr/lirmm/agroportal/omhtrest/OmhtrestApplication.java
+++ src/main/java/fr/lirmm/agroportal/omhtrest/OmhtrestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OmhtrestApplication {
-
+    // test change on code
     public static void main(String[] args) {
         SpringApplication.run(OmhtrestApplication.class, args);
     }
